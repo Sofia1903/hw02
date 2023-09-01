75621b79fc83b78d48455b908464e88d5da32e39
Sofia <116076116+Sofia1903@users.noreply.github.com>
2023-09-01 17:28:30 +0300

    Update HttpError.js

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,14 +1,32 @@
-const messages = {
-    400: "Bad request",
-    401: "Not authorized",
-    403: "Forbidden",
-    404: "Not found!",
-    409: "Conflict",
-  };
-  
-  function HttpErrorCreator(status, message = messages[status]) {
-    const er = new Error(message);
-    er.status = status;
-    return er;
+
+import express from 'express';
+import HttpErrorCreator from './HttpError.js';
+
+const app = express();
+
+function HttpErrorCreator(status, message = messages[status]) {
+  const er = new Error(message);
+  er.status = status;
+  return er;
+}
+export default HttpErrorCreator;
+app.use((err, req, res, next) => {
+  if (err instanceof HttpErrorCreator) {
+    const messages = {
+      400: "Bad request",
+      401: "Not authorized",
+      403: "Forbidden",
+      404: "Not found!",
+      409: "Conflict",
+    };
+    res.status(err.status).json({ error: err.message });
+  } else {
+    
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  export default HttpErrorCreator;
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
