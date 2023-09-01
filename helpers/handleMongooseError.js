2e9fd7b7c61ba4ac3dea220284afaea592281b6d
Sofia <116076116+Sofia1903@users.noreply.github.com>
2023-09-01 18:03:59 +0300

    update

--- helpers/handleMongooseError.js
+++
@@ -1,6 +0,0 @@
-const handleMongooseError = (error, _, next) => {
-  error.status = 404;
-  next();
-};
-
-module.exports = handleMongooseError;
--- models/hooks.js
+++
@@ -1,9 +0,0 @@
-export const handleMongooseError = (er, _, next) => {
-    er.status = er.code === 11000 && er.name === "MongoServerError" ? 409 : 400;
-    next();
-  };
-  
-  export const handleRunValidators = function (next) {
-    this.options.runValidators = true;
-    next();
-  };
