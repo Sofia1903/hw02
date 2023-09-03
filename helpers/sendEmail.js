2c92917985cf8d3e0561746292a85560a95e3c89
Sofia <116076116+Sofia1903@users.noreply.github.com>
2023-09-03 17:30:27 +0300

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -11,16 +11,19 @@ const sendEmail = async data => {
     key: MAILGUN_API_KEY,
   });
 
-  mg.messages
-    .create('sandboxf630bcab1b57429ebde455652c2111da.mailgun.org', {
+  try {
+    const msg = await mg.messages.create('sandboxf630bcab1b57429ebde455652c2111da.mailgun.org', {
       from: 'Mailgun Sandbox <s.skriabina19@gmail.com>',
       to: [data.to],
       subject: 'Verify your email',
       text: 'Verify your email',
       html: data.html,
-    })
-    .then(msg => console.log(msg))
-    .catch(err => console.log(err));
+    });
+    console.log(msg);
+    
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = sendEmail;
